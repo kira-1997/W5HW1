dc6b410d2dba19039db141fdabdf3c0a92166d52
kira <arooji25@live.com>
2023-11-05 21:41:18 +0300

    add

---
+++ java.js
@@ -0,0 +1,14 @@
+let text=document.getElementById("link")
+
+document.getElementById("bt1").addEventListener('click', function(){
+    window.open(text.value,null,'width=375,height=812' )
+})
+document.getElementById("bt2").addEventListener('click', function(){
+    window.open(text.value,null,'width=412,height=869' )
+})
+document.getElementById("bt3").addEventListener('click', function(){
+    window.open(text.value,null,'width=1024,height=1366' )
+})
+document.getElementById("bt4").addEventListener('click', function(){
+    window.open(text.value,null,'width=1920,height=1080' )
+})
